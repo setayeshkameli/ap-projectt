9389ed91c70f4842a8e3dcfb0664674ec55bf613
setayeshkameli <setykameli1384@gmail.com>
2024-06-25 12:46:06 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+});
